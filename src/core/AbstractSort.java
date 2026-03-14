1b1e67b3209980fb13628c111c424ce8fb0fb8f1
GALAXYBOOK5_SP <cspcsp07@naver.com>
2026-03-14 20:10:24 +0900

    feat: add AbstractSort class in core

---
+++ src/core/AbstractSort.java
@@ -0,0 +1,26 @@
+package core;
+
+public abstract class AbstractSort<T extends Comparable<T>> implements Sorter<T> {
+    @Override
+    public abstract void sort(T[] arr);
+
+    @Override
+    public abstract String getName();
+
+    protected boolean less(T v, T w)
+    { return v.compareTo(w) < 0; }
+
+    protected void exch(T[] arr, int i, int j)
+    { T t = arr[i]; arr[i] = arr[j]; arr[j] = t;}
+
+    protected void show(T[] arr){
+        for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    protected boolean isSorted(T[] arr){
+        for (int i = 1; i < arr.length; i++)
+            if (less(arr[i], arr[i-1])) return false;
+        return true;
+    }
+}
--- src/core/Sorter.java
+++ src/core/Sorter.java
@@ -1,6 +1,6 @@
 package core;
 
-public interface sorter<T extends Comparable<T>> {
+public interface Sorter<T extends Comparable<T>> {
     void sort(T[] arr);
     String getName();
 }
